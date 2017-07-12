bdfb30a2e49703e726bd6cc564890a49b1d487e1
Thiago Tomei Fernandez <thiago.tomei@cern.ch>
2017-07-12 10:01:17 -0300

    Better checks for compiling generateJetImage.cc

--- compileGenerateJetImage.sh
+++ compileGenerateJetImage.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
+
+# Check if PYTHIA8DIR is set
+if [ -z ${PYTHIA8DIR+x} ];
+then
+  echo "PYTHIA8DIR is unset, please set it to the location of the PYTHIA8 directory";
+  return -1
+else
+  echo "PYTHIA8DIR is set to '$PYTHIA8DIR'";
+fi
+
+# Check for ROOT
+if hash root-config 2>/dev/null;
+then
+  echo -n "ROOT version: "
+  root-config --version
+else
+  echo "ROOT not found, please install ROOT (https://root.cern.ch)"
+  return -1
+fi
+
+# Check for FASTJET
+if hash fastjet-config 2>/dev/null;
+then
+  echo -n "FASTJET version: "
+  fastjet-config --version
+else
+  echo "FASTJET not found, please install FASTJET (http://fastjet.fr)"
+  return -1
+fi
+
+echo "Compiling..."
 g++ generateJetImage.cc ${PYTHIA8DIR}/lib/libpythia8.a -I${PYTHIA8DIR}/include \
-`root-config --cflags` `root-config --libs` `${FASTJETDIR}/bin/fastjet-config --cxxflags` \
-`${FASTJETDIR}/bin/fastjet-config --libs` \
+`root-config --cflags` `root-config --libs` \
+`fastjet-config --cxxflags` `fastjet-config --libs` \
 -lz -o generateJetImage
+echo "Done"
